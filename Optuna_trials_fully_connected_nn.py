8546b4ae51d77bd9ee9c9ecec1040ca8a6b8fcb4
Indira Ocampo Justiniano <indira.ocampo@csic.es>
2025-05-20 17:51:17 +0200

    Optuna trials for fully connected NN

---
+++ Optuna_trials_fully_connected_nn.py
@@ -0,0 +1,127 @@
+
+import healpy as hp
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import os
+import matplotlib.pyplot as plt
+import optuna
+from healpy import read_map
+tf.keras.backend.clear_session() #clear any previous models
+
+data_directory = "/mnt/lustre/scratch/nlsas/home/csic/eoy/ioj/CMBFeatureNet/data/"
+os.chdir(data_directory)
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' #disable GPU
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  #suppress TF warnings
+print("Current working directory:", os.getcwd())
+
+from astropy.io import fits
+def read_map(file_path):
+    """
+    Reads a Healpy map from a FITS file and flattens the data.
+    """
+    
+    with fits.open(file_path) as hdul:
+        hdul.info()
+        if len(hdul) > 1 and hasattr(hdul[1], 'columns'):
+            print(hdul[1].columns)
+        return np.concatenate(hdul[1].data['T'])
+
+
+#Read the data
+path_lcdm = "./simulated_maps/lcdm/"
+map_temp_data = read_map(path_lcdm + 'cmb_map_0.fits')
+
+#Visualize the map
+nside = hp.npix2nside(len(map_temp_data))
+#nside = 64
+print(f"NSIDE: {nside}")
+
+#Plot
+hp.mollview(map_temp_data, title="Temperature Map", unit="Intensity")
+
+def read_all_maps(path_lcdm, path_feature, n_maps=100):
+    maps = []
+    labels = []
+    
+    #LCDM maps
+    for i in range(n_maps):
+        map_lcdm = read_map(f"{path_lcdm}cmb_map_{i}.fits")
+        maps.append(map_lcdm)
+        labels.append(0)  #lcdm
+    
+    #Feature maps
+    for i in range(n_maps):
+        map_feature = read_map(f"{path_feature}cmb_map_feature_{i}.fits")
+        maps.append(map_feature)
+        labels.append(1)  #feature
+    
+    maps = np.array(maps).astype(np.float32)[..., None]  #Add channel dimension
+    labels = np.array(labels).astype(np.int32)
+    #print(labels)
+    return maps, labels
+
+path_feature = "./simulated_maps/feature/"
+x_raw, y_raw = read_all_maps(path_lcdm, path_feature, n_maps=100) #0: lcdm, 1:feature
+
+maps_model = np.atleast_2d(x_raw)
+print("Shape of maps_model:", maps_model.shape)
+
+#Normalize:
+X = (maps_model - np.mean(maps_model, axis=1, keepdims=True)) / np.std(maps_model, axis=1, keepdims=True)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y_raw, test_size=0.2, random_state=42)
+
+#=== Define objective function for Optuna ===
+def objective(trial):
+    n_layers = trial.suggest_int("n_layers", 1, 4)
+    units = [trial.suggest_int(f"units_l{i}", 64, 512, step=64) for i in range(n_layers)]
+    dropout_rate = trial.suggest_float("dropout_rate", 0.1, 0.5)
+    learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
+    activation = trial.suggest_categorical("activation", ["relu", "tanh", "selu"])
+    optimizer_name = trial.suggest_categorical("optimizer", ["adam", "rmsprop", "sgd"])
+
+    model = models.Sequential()
+    model.add(layers.Input(shape=(X.shape[1],)))
+
+    for u in units:
+        model.add(layers.Dense(u, activation=activation))
+        model.add(layers.Dropout(dropout_rate))
+
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    if optimizer_name == "adam":
+        optimizer = tf.keras.optimizers.Adam(learning_rate)
+    elif optimizer_name == "rmsprop":
+        optimizer = tf.keras.optimizers.RMSprop(learning_rate)
+    else:
+        optimizer = tf.keras.optimizers.SGD(learning_rate)
+
+    model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
+    model.summary(110)
+
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_val, y_val),
+        epochs=100,
+        batch_size=32,
+        verbose=0,
+        callbacks=[tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3)]
+    )
+
+    val_pred = model.predict(X_val).ravel()
+    val_pred_label = (val_pred > 0.5).astype(np.int32)
+    acc = accuracy_score(y_val, val_pred_label)
+
+    return acc  #We want to maximize accuracy
+
+#=== Run optimization ===
+study = optuna.create_study(direction="maximize")
+study.optimize(objective, n_trials=100)
+
+print("Best trial:")
+print(study.best_trial)
+
+print("Best hyperparameters:")
+print(study.best_trial.params)
