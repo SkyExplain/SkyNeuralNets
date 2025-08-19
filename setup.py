d0cf766adc9c51c9942a6aab20cf2054a691fcff
Indira Ocampo Justiniano <indira.ocampo@csic.es>
2025-08-19 18:43:41 +0200

    update setup for package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SkyNeuralNets",
+    version="0.1.0",
+    author="Indira Ocampo",
+    author_email="indira.ocampo@csic.es",
+    description="A Python package for model selection using NNs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/IndiraOcampo/SkyNeuralNets",
+    packages=find_packages(),
+    install_requires=[
+        "numpy", "scipy", "matplotlib", "healpy", "tensorflow", "astropy", "sklearn" #Dependencies
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
