cb69be32117e8ad5df9fd9cf9249111e3f059fda
Indira Ocampo Justiniano <indira.ocampo@csic.es>
2025-08-19 18:44:29 +0200

    update data preprocessing init file

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,26 +1,15 @@
-from .data_preprocessing import covariance_asymmetric_errors
-from .data_preprocessing import generate_camb_power_spectra
-from .data_preprocessing import add_noise_spectrum
-from .data_preprocessing import save_power_spectrum
-from .data_preprocessing import generate_cmb_temperature_map
-from .data_preprocessing import save_cmb_temperature_map
-from .data_preprocessing import PK
-from .data_preprocessing import generate_cmb_polarization_maps
-from .data_preprocessing import save_cmb_polarization_maps
 from .data_preprocessing import read_map
-from .data_preprocessing import deconvolve_gaussian_beam
+from .data_preprocessing import read_all_maps
+from .data_preprocessing import map_to_image
+from .data_preprocessing import compare_maps_spherical
+from .data_preprocessing import compare_maps_patches
+from .data_preprocessing import z_score_norm
 
 __all__ = [
-    "covariance_asymmetric_errors",
-    "generate_camb_power_spectra",
-    "add_noise_spectrum",
-    "save_power_spectrum",
-    "simulate_and_store_cmb_data",
-    "generate_cmb_temperature_map",
-    "save_cmb_temperature_map",
-    "PK",
-    "generate_cmb_polarization_maps",
-    "save_cmb_polarization_maps",
     "read_map",
-    "deconvolve_gaussian_beam"
+    "read_all_maps",
+    "map_to_image",
+    "compare_maps_spherical",
+    "compare_maps_patches",
+    "z_score_norm"
 ]
